86bf4b8751a47174b3e3e76e31f91767fc689aa8
Mohamed Nasir <Mohamednasir.s2006@gmail.com>
2025-03-02 21:52:25 +0530

    Update build error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "970f4647ac82bf19"; }
+/******/ 		__webpack_require__.h = function() { return "c010e63eafd4ffc1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
